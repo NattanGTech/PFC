12424c7e148a74712683e35c747fd3afb8c9a33e
Nattan <nleguyader@gaming.tech>
2022-11-09 15:30:56 +0100

    pfc

--- fonctions.py
+++ fonctions.py
@@ -74,7 +74,7 @@ while tour != 5:
 # Faire un mini jeu qui affige un message lorsque input renvoie le bon caractere
 # le caractere doit etre parametrable
 
-"""
+
 def bonCaractere(caractere):
     # On recupere un caractere aleatoire
     caractereInput = input()
@@ -87,11 +87,11 @@ def bonCaractere(caractere):
 
 def bonCaractereRecursif(caractere):
     caractereInput = input()
-    if carctereInput == caractere:
-        print(reussi)
+    if caractereInput == caractere:
+        print("reussi")
         return
     bonCaractere(caractere)
-"""
+
 
 tab = [1, 2, 3, 8, 0]
 
@@ -112,26 +112,69 @@ stringIntegerValue = str(342) # Renvoie "342" au lieu de 342
 #Exercices
 # Faire une fonction qui concatene 2 chianes de caractere, les separant par une virgule
 
-"""
+
+# Definir une fonction with param chainesA et chaineB
+# qui retourne la concatenation de chaineA, virgule et chaineB
 def concatene(char1, char2):
-    return char1 + ", " char2
-"""
+    # Je m'assure que chaineA soit bien de type str
+    stringfieldChar1 = str(char1)
+    # Je m'assure que chaineB soit bien de type str
+    stringfieldChar2 = str(char2)
+    # Retourner chaineA concatene avec virgule et chaineB
+    return stringfieldChar1 + ", " + stringfieldChar2
+
 
 # Faire une fonction qui itere sur tous les index d'un tab renvoyant une chaine de char avec ensembles des occuration d'un chiffre e.g.:
 #Pour tableau = [0, 1, 1, 1, 0, 1, 1, 0, 1]
 #la fonction(tableau, 0) doit renvoyer "0, 4, 7" n'hesiter pas a implementer la premiere fonction
 
-"""
+# Definir une fonction avec param tab et nb
+# qui retourne une chaine de char avec les index de nb 
 def indexTab(tab, nb):
+    # J'initialise une variable pour contenir la chaine de caractere
     index = ""
+    # Pour tout les index de tab
     for i in range(len(tab) - 1):
-        if tab[i] = nb:
+        # Si index i de tab est equivalent à nb
+        if tab[i] == nb:
+            # Alors Si
             if index == "":
                 index = str(i)
             else:
                 index = concatene(index, str(i))
     return index
-"""
+
+# Correction :
+
+
+#Definir une fonction qui prend une liste tableau et une variable x quelconque
+def findIndexes(tableau, x):
+    #Initialiser i à 0
+    i = 0
+    #Definir chaineResultat en tant que string vide
+    chaineResultat = ""
+    #On Determine firstTurn a True
+    firstTurn = True
+    #Tant que i est inferieur a la longueur de tableau
+    while i < len(tableau):
+        #Alors si l'elt d'index i de tableau est egal a x
+        if tableau[i] == x:
+            #Alors
+            #Si je suis au premier tour ( Si firstTurn est True)
+            if firstTurn:
+                #Alors j'assigne str(i) a chaineResultat
+                chaineResultat = str(i)
+                #On passe firstTurn a False
+                firstTurn = False
+            #Sinon
+            else :
+                #On assigne a chaineResultat le retour de concatene(chaineResultat, str(i))
+                chaineResultat = concatene(chaineResultat, i)
+        #On incremente i de 1
+        i = i + 1
+    #Retourner chaineResultat
+    return chaineResultat
+
 
 #tableauMultiType = ["Yo", True, variable, 4 > 2, None, 145]
 tableauDim = [0, 1, 2, 3]
@@ -163,7 +206,7 @@ listeUtilisateur = {
 # Ecrivez la fonction login(userName, password, listUser) permettant d'afficher un msg de connexion si
 # le combo user/password est bon
 
-"""
+
 def login(userName, password, listUser):
     # Pour chaque cle de listUser
     for e in listUser:
@@ -171,9 +214,21 @@ def login(userName, password, listUser):
         if userName == e and password == listUser[e]:
             # Alors afficher "vous etes connecte"
             print("Vous etes connecte")
-"""
 
-"""
+# Correction
+
+#Definir un fonction login avec comme param une str userName, une str password et une liste listUser
+#qui affiche un message de cpnnexion
+def login(userName, password, listUser):
+    #Si le mdp de userName est egal a password
+    if listUser[userName] == password:
+        #Alors afficher message connexion
+        print("connecté")
+    #Sinon afficher message erreur
+    else :
+        print("identifiant ou mot de passe incorect")
+
+
 def fibonacci(xDebut, lenMax):
     # Pour chaque valeur jusqu'à lenMax
     for i in range(lenMax):
@@ -197,10 +252,9 @@ def fibonacci(xDebut, lenMax):
             suite = concatene(suite, str(xDebut))
     # Je retourne mon resultat
     return suite
-"""
 
-"""
-from random import randint
+
+import random
 
 def tab2Dim(lenTab):
     tab = []
@@ -211,9 +265,13 @@ def tab2Dim(lenTab):
             tab[j].append(random.randint(0, 10))
     return tab
 
-def adjacent(coord1, coord2):
-    for i in range(len(tab)):
-        for j in range(len(tab)):
-            if tab[coord1[1]][coord1[2]] == tab[coord2[1]][coord2[2] - 1]
-
-"""
+def adjacent(coord1, coord2, tab):
+    for i in range(len(tab) - 1):
+        for j in range(len(tab) - 1):
+            if tab[coord1[0]][coord1[1]] == tab[coord2[0]][coord2[1] - 1] or \
+            tab[coord1[0]][coord1[1]] == tab[coord2[0] - 1][coord2[1]] or \
+            tab[coord1[0]][coord1[1]] == tab[coord2[0] + 1][coord2[1]] or \
+            tab[coord1[0]][coord1[1]] == tab[coord2[0]][coord2[1] + 1]:
+                print("Oui")
+                return
+    print("non")
