2ea085f7dec4800590c1f2c07f1299c49fb29d98
Nattan <nleguyader@gaming.tech>
2022-11-10 18:55:37 +0100

    PFC_Final

--- PFC.py
+++ PFC.py
@@ -6,7 +6,7 @@ On admet une fonction input qui recupere l'entree d'un joueur
 
 Definir une fonction pierreFeuilleCiseau sans parametre
 qui retourne un message pour savoir si l'utilisateur à gagne, perdu ou egalite
-    Assigne a la variable bestOff le retour de l'execution de la fonction input avec en parametre le message "Le premier à combien ?"
+    Assigne a la variable bestOff le retour de l'execution de la fonction input avec en parametre le message "Le premier à combien ?" transformer en integer
     Initialiser la variable scoreJoueur a 0
     Initialiser la variable scoreBot a 0
     Initialiser la variable nbManche a 0
@@ -15,8 +15,8 @@ qui retourne un message pour savoir si l'utilisateur à gagne, perdu ou egalite
         Incrementer la variable nbManche de 1
         Assigne a la variable choixJoueur le retour de l'execution de la fonction input avec en parametre le message "Pierre, Feuille, Ciseau ?"
         Tant que le choixJoueur n'est pas egal a "Pierre" et n'est pas egal a "Feuille" et n'est pas egal a "Ciseau"
-            Alors assigne a la variable choixJoueur le retour de l'execution de la fonction input avec en parametre le message "Pierre, Feuille, Ciseau ?"
-        Definir une liste choixPossible qui comprend "Pierre", "Feuille" et "Ciseau"
+            Alors assigne a la variable choixJoueur le retour de l'execution de la fonction input avec en parametre le message "Incorrect ! Pierre, Feuille, Ciseau ?"
+        Assigne a la variable choixPossible une liste qui comprend "Pierre", "Feuille" et "Ciseau"
         Assigne a la variable choixBot la correspondance dans la liste choixPossible du retour de l'execution de la fonction random avec comme parametre 0 et 2
         Si le choixjouer est egal au choixBot
             Alors
@@ -38,6 +38,8 @@ qui retourne un message pour savoir si l'utilisateur à gagne, perdu ou egalite
     Assigne a la variable restart le retour de l'execution de la fonction input avec en parametre le message "Voulez-vous rejouer ? Oui / Non"
     Si le restart est egal a "Oui"
         Alors reexecuter la fonction pierreFeuilleCiseau()
+
+Execution de la fonction pierreFeuilleCiseau()
 
 FIN
 """
